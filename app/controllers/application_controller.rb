c7887a9c77e596fd6c19152bfd934e2a4189fe17
kanieksuke <95512847+kanieksuke@users.noreply.github.com>
2022-03-04 17:29:25 +0900

    バリデーション設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,5 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:target_amount, :target_date, :income, :fixed_cost])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  with_options presence: true do
+    validates :target_amount, numericality: {only_integer: true, greater_than_or_equal_to: 1}
+    validates :target_date, numericality: {only_integer: true, greater_than_or_equal_to: 1}
+    validates :income, numericality: {only_integer: true}
+    validates :fixed_cost, numericality: {only_integer: true}
+  end
 end
